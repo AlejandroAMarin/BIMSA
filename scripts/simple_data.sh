b56b6da1dc6127cb370e71735a181e2a51caaa2e
Alejandro Alonso <aalonso@upmemcloud5.cloud.upmem.com>
2024-04-08 12:44:13 +0200

    missing folder in master commit

--- scripts/simple_data.sh
+++ scripts/simple_data.sh
@@ -11,9 +11,9 @@ for i in 1 2 3
 do
     cd	/home/upmem0046/aalonso/ulsapim-github/ULSAPIM/upmem/
         make clean
-        make NR_TASKLETS=$tl NR_DPUS=$NR_DPUS PERF=1 DYNAMIC=1 MAX_DISTANCE_THRESHOLD=100000
+        make NR_TASKLETS=$tl NR_DPUS=$NR_DPUS DYNAMIC=0 MAX_DISTANCE_THRESHOLD=50000
     echo "\n Running experiment for NR_TASKLETS=$tl NR_DPUS=$NR_DPUS file=$1 repetition=$i\n" >> $EXP_FILE
-        ./bin/ulsapim_host -i /home/upmem0046/aalonso/inputs/$1 -s "$2" >> $EXP_FILE
+        ./bin/bimsa_host -i /home/upmem0046/aalonso/inputs/$1 -s "$2" >> $EXP_FILE
 done
 done
 
